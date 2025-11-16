e845f58995ad8312bede5516400d7522a12b6551
Kyypie69 <markyyroblox@gmail.com>
2025-11-16 19:03:53 +0800

    Update Kyyyy.lua

--- Kyyyy.lua
+++ Kyyyy.lua
@@ -23,7 +23,7 @@ local Window = Library:CreateWindow({
     SubTitle = "Version 6.9 | by Markyy",
     Size = UDim2.fromOffset(500, 335),
     TabWidth = 150,
-    Theme = "SkyBlue",
+    Theme = "DarkBlue",
     Acrylic = false,
 })
 
