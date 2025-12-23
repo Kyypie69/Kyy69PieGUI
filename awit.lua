b3d4c22f58e2d678f69c2faa2e2e133b3234bd63
Kyypie69 <markyyroblox@gmail.com>
2025-12-23 19:08:40 +0800

    Rename OBFUSCATED.lua to awit.lua

---
+++ awit.lua
@@ -0,0 +1,10 @@
+--[[  EleriumV2xKYY  (ver.69)  ]]
+local Library = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/Kyypie69/Library.UI/refs/heads/main/KYY.luau"))()
+
+local Win = Library.new({
+    MainColor      = Color3.fromRGB(138,43,226),
+    ToggleKey      = Enum.KeyCode.Insert,
+    MinSize        = Vector2.new(450,320),
+    CanResize      = false
+})
